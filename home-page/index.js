4966425755b55490b2ac3f68d98b47d994a408b8
Majekodunmi Pelumi <pmajekodunmi3@gmail.com>
2024-03-22 02:30:47 -0700

    my first commit

---
+++ home-page/index.js
@@ -0,0 +1,71 @@
+//number
+//this range from 0 to infinity
+const pelumi = 123435445;
+
+//string
+// string is a sequence of characters within double quotes
+const mySelf = "I am a self-taught developer";
+
+//boolean
+const isAwesome = false;
+
+//checking if isAwesome is true
+if (isAwesome === true) {
+  console.log("I am not awesome");
+} else if (isAwesome === false) {
+  console.log("I am awesome");
+}
+
+//null
+const akinStorage = null;
+
+//undefined
+const akin = undefined;
+
+//arrayss
+// [] is an array
+// arrays are list of values
+// arrays are zero-indexed
+// arrays can be of different types of values
+const akinArray = [
+  1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+];
+const pelumiArray = ["a", "b", "c", "d", "e"];
+const setArr = ["a", 1];
+
+// an object
+
+const attendantA = {
+  attendantName: "Pelumi",
+  attendantAddress: "Ikotun",
+  attendantGender: "Female",
+  attendantAge: 90,
+  attendantComplexion: "Dark",
+};
+
+const attendantB = {
+  attendantName: "Akin",
+  attendantAddress: "Berger",
+  attendantGender: "Male",
+  attendantAge: 90,
+  attendantComplexion: "Dark",
+};
+
+const arrayOfAttendants = [attendantA, attendantB];
+
+const testAttendantsArray = [
+  {
+    attendantName: "Pelumi",
+    attendantAddress: "Ikotun",
+    attendantGender: "Female",
+    attendantAge: 90,
+    attendantComplexion: "Dark",
+  },
+  {
+    attendantName: "Akin",
+    attendantAddress: "Berger",
+    attendantGender: "Male",
+    attendantAge: 90,
+    attendantComplexion: "Dark",
+  },
+];
